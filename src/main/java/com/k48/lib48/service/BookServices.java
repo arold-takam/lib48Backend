2136401d0b8f3748b8cb06fcd396ba5b07cf3a0a
arold-takam <www.aroldtakam3@gmail.com>
2025-09-03 10:26:30 +0100

    returning management v5

--- src/main/java/com/k48/lib48/service/BookServices.java
+++ src/main/java/com/k48/lib48/service/BookServices.java
@@ -20,129 +20,128 @@ import java.util.Optional;
 
 @Service
 public class BookServices {
+	
+	private final BookRespositories bookRespo;
+	private final CategoryRepositories categoryRepo;
+	private final FileStorageService fileStorageService;
+	private final HistoryService historyService;
+	
+	public BookServices(BookRespositories bookRespo, CategoryRepositories categoryRepo ,FileStorageService fileStorageService, HistoryService historyService) {
+		this.bookRespo = bookRespo;
+		this.categoryRepo = categoryRepo;
+		this.fileStorageService= fileStorageService;
+		this.historyService = historyService;
+	}
 
-    private final BookRespositories bookRespo;
-    private final CategoryRepositories categoryRepo;
-    private final FileStorageService fileStorageService;
-    private final HistoryService historyService;
+//    ---------------------------------------------------------------------------------------------------------------------------------------------------
+	
+	public List<Book> getAllBooks() {
+		return bookRespo.findAll();
+	}
+	
+	public Book getBookId(long id) {
+		return bookRespo.findById(id)
+			       .orElseThrow(() -> new NoSuchElementException("Book not found"));
+	}
+	
+	public Book getBooksByTitle(String title) {
+		Book book = bookRespo.findByTitreIgnoreCase(title);
+		if (book == null) {
+			throw new NoSuchElementException("Book not found");
+		}
+		return book;
+	}
+	
+	public List<Book> getBooksByCategorieNom(String nomCategorie) {
+		Category category = categoryRepo.findByNomIgnoreCase(nomCategorie);
+		if (category == null) {
+			throw new NoSuchElementException("Category not found");
+		}
+		return bookRespo.findAllByCategory(category);
+	}
+	
+	@Transactional
+	public Book createBook(long idCategory, BookRequestDTO   bookRequestDTO, MultipartFile coverImage) {
 
-    public BookServices(BookRespositories bookRespo, CategoryRepositories categoryRepo ,FileStorageService fileStorageService, HistoryService historyService) {
-        this.bookRespo = bookRespo;
-        this.categoryRepo = categoryRepo;
-        this.fileStorageService= fileStorageService;
-        this.historyService = historyService;
-    }
+// Validation du titre: il ne peut être ni null ni vide
+		if (bookRequestDTO == null || bookRequestDTO.titre() == null || bookRequestDTO.titre().isBlank()) {
+			throw new IllegalArgumentException("The Title cannot be null or empty");
+		}
 
-    public List<Book> getAllBooks() {
-        return bookRespo.findAll();
-    }
+//Trouver la categorie
+		Optional<Category> categoryOpt = categoryRepo.findById(idCategory);
+		if (categoryOpt.isEmpty()) {
+			throw new NoSuchElementException("Category not found");
+		}
+		
+		Category category = categoryOpt.get();
+		
+		Book book = new Book();
+		
+		
+		HistoryDTO historyGerantTrue = new HistoryDTO("Gerant",book.getTitre(),"CREATE","SUCCESS", LocalDateTime.now(),"Echec de la création du livre couverture non validé");
+		HistoryDTO historyGerantFalse = new HistoryDTO("Gerant",book.getTitre(),"CREATE","FAILED", LocalDateTime.now(),"Création du livre réussi");
 
-    public Book getBookId(long id) {
-        return bookRespo.findById(id)
-                .orElseThrow(() -> new NoSuchElementException("Book not found"));
-    }
-
-    public Book getBooksByTitle(String title) {
-        Book book = bookRespo.findByTitreIgnoreCase(title);
-        if (book == null) {
-            throw new NoSuchElementException("Book not found");
-        }
-        return book;
-    }
-
-    public List<Book> getBooksByCategorieNom(String nomCategorie) {
-        Category category = categoryRepo.findByNomIgnoreCase(nomCategorie);
-        if (category == null) {
-            throw new NoSuchElementException("Category not found");
-        }
-        return bookRespo.findAllByCategory(category);
-    }
-
-
-    @Transactional
-
-    public Book createBook(long idCategory, BookRequestDTO   bookRequestDTO, MultipartFile coverImage) {
-        
-        // Validation du titre: il ne peut être ni null ni vide
-        if (bookRequestDTO == null || bookRequestDTO.titre() == null || bookRequestDTO.titre().isBlank()) {
-            throw new IllegalArgumentException("The Title cannot be null or empty");
-        }
-        
-        //Trouver la categorie
-        Optional<Category> categoryOpt = categoryRepo.findById(idCategory);
-        if (categoryOpt.isEmpty()) {
-            throw new NoSuchElementException("Category not found");
-        }
-        
-        Category category = categoryOpt.get();
-        
-        Book book = new Book();
-
-
-        HistoryDTO historyGerantTrue = new HistoryDTO("Gerant",book.getTitre(),"CREATE","SUCCESS", LocalDateTime.now(),"Echec de la création du livre couverture non validé");
-        HistoryDTO historyGerantFalse = new HistoryDTO("Gerant",book.getTitre(),"CREATE","FAILED", LocalDateTime.now(),"Création du livre réussi");
-
-        //valider l'image de couverture
-        if (coverImage != null && !coverImage.isEmpty()) {
-            String fileName = fileStorageService.storeFile(coverImage, book.getId() );
-            book.setCoverImage(fileName);
-        }else {
-            historyService.createHistory(historyGerantFalse);
-        }
-        book.setTitre(bookRequestDTO.titre());
-        book.setAuteur(bookRequestDTO.auteur());
-        book.setEstDisponible(true);
-        book.setEditeur(bookRequestDTO.editeur());
-        book.setEtatLivre(EtatLivre.NEUF);
-        book.setCategory(category);
-
-        historyService.createHistory(historyGerantTrue);
-        
-        categoryRepo.save(category);
-       return bookRespo.save(book);
-    }
-
-    public Book updateBook(long id, EtatLivre livreEtat , long idCategory ,BookUpDateDTO bookUpDateDTO, MultipartFile coverImage) {
-        Book existingBook = bookRespo.findById(id)
-                .orElseThrow(() -> new NoSuchElementException("Book not found"));
-
-        HistoryDTO historyGerantTrue = new HistoryDTO("Gerant",existingBook.getTitre(),"UPDATE","SUCCESS", LocalDateTime.now(),"Echec de la mise à jour du livre ");
-        HistoryDTO historyGerantFalse = new HistoryDTO("Gerant",existingBook.getTitre(),"UPDATE","FAILED", LocalDateTime.now(),"Mise à jour du livre réussi");
-        
-        Optional<Category> category =  categoryRepo.findById(idCategory);
-        if (category.isEmpty()){
-            historyService.createHistory(historyGerantFalse);
-            throw new NoSuchElementException("Category not found ");
-        }
-
-        if (coverImage != null && !coverImage.isEmpty()) {
-            fileStorageService.deleteFile(existingBook.getCoverImage());
-        }
-        String fileName = fileStorageService.storeFile(coverImage, existingBook.getId() );
-        existingBook.setCoverImage(fileName);
-        existingBook.setTitre(bookUpDateDTO.titre());
-        existingBook.setAuteur(bookUpDateDTO.auteur());
-        existingBook.setEstDisponible(bookUpDateDTO.estDisponible());
-        existingBook.setEditeur(bookUpDateDTO.editeur());
-        existingBook.setEtatLivre(livreEtat);
-        existingBook.setCategory(category.get());
-
-        historyService.createHistory(historyGerantTrue);
-        
-        return bookRespo.save(existingBook);
-    }
-
-    public void deleteBook(long id) {
-
-        HistoryDTO historyGerantTrue = new HistoryDTO("Gerant",bookRespo.findById(id).get().getTitre(),"DELETE","SUCCESS", LocalDateTime.now(),"Echec de la supression du livre ");
-        HistoryDTO historyGerantFalse = new HistoryDTO("Gerant",bookRespo.findById(id).get().getTitre(),"DELETE","FAILED", LocalDateTime.now(),"Supression du livre réussi");
-
-        if (!bookRespo.existsById(id)) {
-            historyService.createHistory(historyGerantFalse);
-            throw new NoSuchElementException("Book not found");
-        }
-
-        historyService.createHistory(historyGerantTrue);
-        bookRespo.deleteById(id);
-    }
+//valider l'image de couverture
+		if (coverImage != null && !coverImage.isEmpty()) {
+			String fileName = fileStorageService.storeFile(coverImage, book.getId() );
+			book.setCoverImage(fileName);
+		}else {
+			historyService.createHistory(historyGerantFalse);
+		}
+		book.setTitre(bookRequestDTO.titre());
+		book.setAuteur(bookRequestDTO.auteur());
+		book.setEstDisponible(true);
+		book.setEditeur(bookRequestDTO.editeur());
+		book.setEtatLivre(EtatLivre.NEUF);
+		book.setCategory(category);
+		
+		historyService.createHistory(historyGerantTrue);
+		
+		categoryRepo.save(category);
+		return bookRespo.save(book);
+	}
+	
+	public Book updateBook(long id, EtatLivre livreEtat , long idCategory ,BookUpDateDTO bookUpDateDTO, MultipartFile coverImage) {
+		Book existingBook = bookRespo.findById(id)
+			                    .orElseThrow(() -> new NoSuchElementException("Book not found"));
+		
+		HistoryDTO historyGerantTrue = new HistoryDTO("Gerant",existingBook.getTitre(),"UPDATE","SUCCESS", LocalDateTime.now(),"Echec de la mise à jour du livre ");
+		HistoryDTO historyGerantFalse = new HistoryDTO("Gerant",existingBook.getTitre(),"UPDATE","FAILED", LocalDateTime.now(),"Mise à jour du livre réussi");
+		
+		Optional<Category> category =  categoryRepo.findById(idCategory);
+		if (category.isEmpty()){
+			historyService.createHistory(historyGerantFalse);
+			throw new NoSuchElementException("Category not found ");
+		}
+		
+		if (coverImage != null && !coverImage.isEmpty()) {
+			fileStorageService.deleteFile(existingBook.getCoverImage());
+		}
+		String fileName = fileStorageService.storeFile(coverImage, existingBook.getId() );
+		existingBook.setCoverImage(fileName);
+		existingBook.setTitre(bookUpDateDTO.titre());
+		existingBook.setAuteur(bookUpDateDTO.auteur());
+		existingBook.setEstDisponible(bookUpDateDTO.estDisponible());
+		existingBook.setEditeur(bookUpDateDTO.editeur());
+		existingBook.setEtatLivre(livreEtat);
+		existingBook.setCategory(category.get());
+		
+		historyService.createHistory(historyGerantTrue);
+		
+		return bookRespo.save(existingBook);
+	}
+	
+	public void deleteBook(long id) {
+		HistoryDTO historyGerantTrue = new HistoryDTO("Gerant",bookRespo.findById(id).get().getTitre(),"DELETE","SUCCESS", LocalDateTime.now(),"Echec de la supression du livre ");
+		HistoryDTO historyGerantFalse = new HistoryDTO("Gerant",bookRespo.findById(id).get().getTitre(),"DELETE","FAILED", LocalDateTime.now(),"Supression du livre réussi");
+		
+		if (!bookRespo.existsById(id)) {
+			historyService.createHistory(historyGerantFalse);
+			throw new NoSuchElementException("Book not found");
+		}
+		
+		historyService.createHistory(historyGerantTrue);
+		bookRespo.deleteById(id);
+	}
 }
