a8afa9b7ff086a1e8add8e7dacffd41771f6f58b
arold-takam <www.aroldtakam3@gmail.com>
2026-01-02 11:51:27 +0100

    starting CICD test 1

--- src/test/java/com/k48/lib48/controller/integration/ReturnBookIntegrationTest.java
+++ src/test/java/com/k48/lib48/controller/integration/ReturnBookIntegrationTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -28,6 +29,7 @@ import org.testcontainers.containers.PostgreSQLContainer;
 @AutoConfigureMockMvc
 @Transactional
 @Testcontainers
+@ActiveProfiles("test")
 public class ReturnBookIntegrationTest {
 	
 	@Container // Démarre un vrai Postgres
