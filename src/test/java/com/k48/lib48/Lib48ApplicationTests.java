43b0a4a91640b8e1e7c24c9dd07c9a79ea8913aa
arold-takam <www.aroldtakam3@gmail.com>
2026-01-02 12:13:52 +0100

    starting CICD test 4

--- src/test/java/com/k48/lib48/Lib48ApplicationTests.java
+++ src/test/java/com/k48/lib48/Lib48ApplicationTests.java
@@ -2,8 +2,10 @@ package com.k48.lib48;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Lib48ApplicationTests {
 
 	@Test
