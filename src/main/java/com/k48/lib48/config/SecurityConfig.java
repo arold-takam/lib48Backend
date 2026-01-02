6650523d93e45343d0d6906f264a3b524b5672ef
arold-takam <www.aroldtakam3@gmail.com>
2026-01-02 15:49:25 +0100

    Fix: cors repearing

--- src/main/java/com/k48/lib48/config/SecurityConfig.java
+++ src/main/java/com/k48/lib48/config/SecurityConfig.java
@@ -96,15 +96,20 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
+		
+		// On met les deux pour être sûr
+		configuration.setAllowedOrigins(Arrays.asList(
+			"http://localhost:5500",
+			"http://127.0.0.1:5500"
+		));
+		
 		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-		configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+		configuration.setAllowedHeaders(Arrays.asList("*")); // Autorise tous les headers
 		configuration.setAllowCredentials(true);
 		
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
-	
 	
 }
